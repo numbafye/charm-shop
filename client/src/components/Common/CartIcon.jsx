7e99404afae2770e3aef26953a521464d761945f
numbafye <saadiqgp@gmail.com>
2023-12-13 09:53:53 -0500

    corrected CartIcon jsx toggleCart

--- client/src/components/Common/CartIcon.jsx
+++ client/src/components/Common/CartIcon.jsx
@@ -4,17 +4,16 @@ import Cart from "../../pages/Cart";
 function CartIcon() {
   const { cartCounter, toggleCart, isCartVisible } = useCart();
 
-  const handleCartIconClick = (e) => {
-    e.stopPropagation(); // Prevent the click event from reaching the body
-    toggleCart();
-    console.log("I WORK??")
-  };
-
   return (
     <div className="mr-3">
-      <box-icon onClick={handleCartIconClick} className="cart" type="solid" name="shopping-bag"></box-icon>
+      <box-icon
+        onClick={toggleCart}
+        className="cart"
+        type="solid"
+        name="shopping-bag"
+      ></box-icon>
       <span id="cart-counter">{cartCounter}</span>
-      {isCartVisible && <Cart/>}
+      {isCartVisible && <Cart />}
     </div>
   );
 }
