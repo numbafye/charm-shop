71d0cf24184d12b7e9064f9ac98fb8bba1ccc3fb
numbafye <saadiqgp@gmail.com>
2024-03-27 21:06:14 -0400

    added new schema for header

---
+++ client/charmecom/schemas/about.js
@@ -0,0 +1,14 @@
+//ABOUT SECTION SCHEMA
+
+export default {
+  name: 'about',
+  title: 'About',
+  type: 'document',
+  fields: [
+    {
+      name: 'desc',
+      title: 'Desc',
+      type: 'string',
+    },
+  ],
+}
---
+++ client/charmecom/schemas/header.js
@@ -0,0 +1,29 @@
+//HEADER PRODUCT SCHEMA
+
+export default {
+  name: 'header',
+  title: 'Header',
+  type: 'document',
+  fields: [
+    {
+      name: 'product',
+      title: 'Product',
+      type: 'string',
+    },
+    {
+      name: 'desc',
+      title: 'Desc',
+      type: 'string',
+    },
+    {
+      name: 'discount',
+      title: 'Discount',
+      type: 'string',
+    },
+    {
+      name: 'saleTime',
+      title: 'SaleTime',
+      type: 'string',
+    },
+  ],
+}
