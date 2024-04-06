31ae4e2f97bfb9a8b9207480fa31846736b9525a
numbafye <saadiqgp@gmail.com>
2024-04-06 13:04:04 -0400

    banner data added to header return statement

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -30,6 +30,13 @@ function Header() {
     <section className="Header">
       <div className=" mx-auto">
         <div className="banner flex justify-center">
+          {bannerData && (
+            <img
+              className="w-1/2 h-96 rounded-sm"
+              src={bannerData.image.asset.url}
+              alt="Banner"
+            />
+          )}
           <h1 className="Shop w-28 mx-auto bg-accent text-center mt-4 z-20 md:w-48">
             <Link to="/products">SHOP NOW</Link>
           </h1>
